8d45b68f7088567ed2f9e10abbbd4acfa4bc748a
Mark Ungvari <ungvarimark@gmail.com>
2019-12-04 12:11:25 +0100

    Initialize excercise

--- BFS-c-sharp/Program.cs
+++ BFS-c-sharp/Program.cs
@@ -19,5 +19,26 @@ namespace BFS_c_sharp
             Console.WriteLine("Done");
             Console.ReadKey();
         }
+
+        private int ShortestDistanceBetweenUsers(UserNode user1, UserNode user2)
+        {
+            return 0;
+        }
+
+        private void ListFriendsOfFriendsAtDistance(UserNode user, int ditance)
+        {
+            List<UserNode> friendsOfFriends = new List<UserNode>();
+
+            foreach(var friend in friendsOfFriends)
+            {
+                Console.WriteLine($"{friend.FirstName} {friend.LastName} [{friend.Id}]");
+            }
+        }
+
+        // BONUS EXCERCISE
+        //private void ShortestPaths(UserNode user1, UserNode user2)
+        //{
+
+        //}
     }
 }
